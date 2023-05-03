ab7fa50899799dc0fe377729a39f3f9c5d41c627
LAPTOP-L82MDLB4\user <tsurubaso@gmail.com>
2023-05-03 22:53:07 +0900

    pas march

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const express =require ("express");
 const app=express();
 const mongoose=require("mongoose");
 
+
+mongoose.connect("mongodb+srv://sb:123@cluster0.mongodb.net/test?retryWrites=true&w=majority").then(()=> {console.log("Connecté")});
+
+
 app.listen(3000,()=>{
 console.log("Server is server")
 
-})
+});
+
